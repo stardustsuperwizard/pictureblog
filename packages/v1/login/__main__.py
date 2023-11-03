022e43e289e2eedd7cc83349205efed821e5b4f4
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2023-11-03 13:40:43 -0400

    thing

--- packages/v1/login/__main__.py
+++ packages/v1/login/__main__.py
@@ -2,20 +2,38 @@ import json
 import logging
 
 import jinja2
+import jwt
+
+
+SECRET = 'mysecret'
+
+
+def create_jwt(user: str, password: str):
+    encoded_jwt = jwt.encode({'user': user}, SECRETKEY, algorithm='HS256')
+    return encoded_jwt
+
+
+def validate_jwt(jwt: str):
+    data = jwt.decode(jwt, SECRET, algorithms=['HS256'])
+    return data
 
 
 def html_reponse(event):
     method = event.get('http', {}).get('headers', {}).get("method", "")
     if method.lower() == 'post':
+        jwt = create_jwt(event['username'], event['password'])
         environment = jinja2.Environment(loader=jinja2.FileSystemLoader("templates/"))
-        template = environment.get_template("base.html")
+        template = environment.get_template("authenticated.html")
         return {
             "statusCode": 200,
-            "body": template.render(event = json.dumps(event))
+            "body": template.render(event = json.dumps(event)),
+            "headers": {
+                "Set-Cookie": f"Auth={jwt}"
+            }
         }    
     else:
         environment = jinja2.Environment(loader=jinja2.FileSystemLoader("templates/"))
-        template = environment.get_template("base.html")
+        template = environment.get_template("unauthenticated.html")
         return {
             "statusCode": 200,
             "body": template.render(event = json.dumps(event))
