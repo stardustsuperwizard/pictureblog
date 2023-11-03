c85e7e6514a6b51267eddd71fc3eccf8ef7332ad
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2023-11-02 22:54:59 -0400

    more changes

--- packages/api/index/__main__.py
+++ packages/api/index/__main__.py
@@ -1,10 +1,15 @@
+import json
 import logging
 
 
 def main(event, context):
     status = 200
+    if "text/html" in event.get('http', {}).get('headers', {}).get("accept", ""):
+        response = f"<html><body><h1>{json.dumps(event, indent=4)}</h1></body></html>"
+    else:
+        response = {"message": event}
     return {
         "statusCode": status,
-        "body": event
+        "body": response
     }
 
